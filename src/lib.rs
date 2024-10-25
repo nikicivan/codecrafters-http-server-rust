bf9eb4997fa1f120cb38fcedc11264a6d20360e6
nikicivan <ivan.nikic@gmail.com>
2024-10-25 19:40:02 +0200

    return a file

---
+++ src/lib.rs
@@ -0,0 +1,153 @@
+use std::{
+    fs,
+    sync::{mpsc, Arc, Mutex},
+    thread,
+};
+
+#[derive(Debug)]
+pub struct Response {
+    status_code: String,
+    status_message: String,
+    headers: String,
+    body: String,
+}
+
+impl Response {
+    pub fn new(
+        status_code: String,
+        status_message: String,
+        headers: String,
+        body: String,
+    ) -> Response {
+        let headers = if !headers.is_empty() && !headers.ends_with("\r\n") {
+            format!("{headers}\r\n")
+        } else {
+            headers
+        };
+        Response {
+            status_code,
+            status_message,
+            body,
+            headers,
+        }
+    }
+
+    pub fn add_header(&mut self, key: &str, value: &str) {
+        self.headers.push_str(&format!("{key}: {value}\r\n"));
+    }
+
+    pub fn set_body(&mut self, body: String) {
+        self.body = body;
+    }
+
+    pub fn create_http_response(&self) -> Result<Vec<u8>, ()> {
+        if self.status_code.is_empty() || self.status_message.is_empty() {
+            ()
+        }
+        let response = format!(
+            "HTTP/1.1 {} {}\r\n{}\r\n{}",
+            self.status_code, self.status_message, self.headers, self.body
+        );
+        let result = response.into_bytes();
+
+        Ok(result)
+    }
+
+    pub fn not_found() -> Response {
+        Response::new(
+            String::from("404"),
+            String::from("Not Found"),
+            String::from("Content-Length: 0"),
+            String::new(),
+        )
+    }
+
+    pub fn create_response_with_body(body: &str) -> Response {
+        let length = &body.len();
+        let mut response = Response::new(
+            String::from("200"),
+            String::from("OK"),
+            String::from(""),
+            String::from(body),
+        );
+        response.add_header("Content-Type", "text/plain");
+        response.add_header("Content-Length", &length.to_string());
+        response
+    }
+
+    pub fn create_file_response(path: &str) -> Response {
+        let content = fs::read_to_string(&path);
+        match content {
+            Ok(c) => {
+                let size = fs::metadata(path).unwrap().len();
+                let mut response =
+                    Response::new(String::from("200"), String::from("OK"), String::new(), c);
+                response.add_header("Content-Type", "application/octet-stream");
+                response.add_header("Content-Length", &size.to_string());
+                response
+            }
+            Err(_) => Response::not_found(),
+        }
+    }
+}
+
+static INITIAL_WORKER_AMOUNT: usize = 4;
+
+struct Worker {
+    id: usize,
+    thread: Option<thread::JoinHandle<()>>,
+}
+
+type Job = Box<dyn FnOnce() + Send + 'static>;
+
+impl Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        let thread = thread::spawn(move || loop {
+            let message = receiver.lock().unwrap().recv();
+            match message {
+                Ok(job) => {
+                    println!("Worker {id} got a job. Executing...");
+                    job();
+                }
+                Err(_) => {
+                    println!("Worker {id} disconnected. Shuting down...");
+                    break;
+                }
+            }
+        });
+        Worker {
+            id,
+            thread: Some(thread),
+        }
+    }
+}
+
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+    sender: Option<mpsc::Sender<Job>>,
+}
+
+impl ThreadPool {
+    pub fn new() -> ThreadPool {
+        let (sender, receiver) = mpsc::channel();
+        let receiver = Arc::new(Mutex::new(receiver));
+
+        let mut workers = Vec::with_capacity(INITIAL_WORKER_AMOUNT);
+        for i in 0..INITIAL_WORKER_AMOUNT {
+            let worker = Worker::new(i, Arc::clone(&receiver));
+            workers.push(worker);
+        }
+        ThreadPool {
+            workers,
+            sender: Some(sender),
+        }
+    }
+
+    pub fn execute<F>(&self, f: F)
+    where
+        F: FnOnce() + Send + 'static,
+    {
+        let job = Box::new(f);
+        self.sender.as_ref().unwrap().send(job).unwrap();
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,130 +1,79 @@
+use codecrafters_http_server::{Response, ThreadPool};
+use std::io::Write;
+use std::net::TcpListener;
 use std::{
-    io::{BufRead, BufReader, Write},
-    net::{TcpListener, TcpStream},
+    collections::HashMap,
+    env,
+    io::{BufRead, BufReader, BufWriter},
+    net::TcpStream,
 };
-use threadpool::ThreadPool;
-
-#[derive(Debug)]
-enum HttpRequestMethod {
-    Get,
-    Post,
-}
-
-impl HttpRequestMethod {
-    fn from_string(input: &str) -> Option<HttpRequestMethod> {
-        match input {
-            "GET" => Some(HttpRequestMethod::Get),
-            "POST" => Some(HttpRequestMethod::Post),
-            _ => None,
-        }
-    }
-}
-
-#[derive(Debug)]
-struct HttpHeader<'a> {
-    name: &'a str,
-    value: &'a str,
-}
-
-#[derive(Debug)]
-struct HttpRequest<'a> {
-    method: HttpRequestMethod,
-    version: &'a str,
-    path: &'a str,
-    headers: Vec<HttpHeader<'a>>,
-}
-
-impl<'a> HttpRequest<'a> {
-    pub fn new(request_line: &'a str, header_lines: &'a [String]) -> Self {
-        let split_line: Vec<_> = request_line.split_whitespace().collect();
-        println!("New HTTP Request {:?}", split_line.clone());
-        let mut headers: Vec<HttpHeader<'a>> = Vec::new();
-        for header in header_lines {
-            if let Some((key, value)) = header.split_once(":") {
-                headers.push(HttpHeader {
-                    name: key.trim(),
-                    value: value.trim(),
-                });
-            }
-        }
-        return Self {
-            method: HttpRequestMethod::from_string(split_line[0])
-                .expect("Unable to parse http method"),
-            version: split_line[2],
-            path: split_line[1],
-            headers,
-        };
-    }
-}
 
 fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
-    let pool = ThreadPool::new(4);
-
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
+    println!("Server started listening at 127.0.0.1:4221");
+
+    let thread_pool = ThreadPool::new();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(stream) => {
-                pool.execute(|| {
-                    handle_connection(stream);
-                });
+            Ok(s) => {
+                println!("accepting stream connection");
+                thread_pool.execute(|| handle_connection(s));
             }
-            Err(stream_error) => {
-                println!("stream error: {}", stream_error);
+            Err(e) => {
+                println!("error: {}", e);
             }
         }
     }
 }
 
 fn handle_connection(mut stream: TcpStream) {
-    let buf_reader = BufReader::new(&mut stream);
+    let buf_reader = BufReader::new(&stream);
 
-    let request_line: Vec<_> = buf_reader
+    let request: Vec<String> = buf_reader
         .lines()
-        .map(|result| result.unwrap())
+        .map(|line| line.unwrap())
         .take_while(|line| !line.is_empty())
         .collect();
 
-    // let response = match request_line.as_str() {
-    //     "GET / HTTP/1.1" => "HTTP/1.1 200 OK\r\n\r\n",
-    //     _ => "HTTP/1.1 404 Not Found\r\n\r\n",
-    // };
+    let request_line = &request[0];
+    let mut request_headers = HashMap::new();
 
-    println!("Request line {:#?}", request_line.clone());
-
-    let http_request = HttpRequest::new(&request_line[0], &request_line[1..]);
-    let response: String;
-    let mut path_segments = http_request.path.split("/");
-    if let Some(endpoint) = path_segments.nth(1) {
-        println!("Response: {:#?}", endpoint);
-        response = match endpoint {
-            "" => String::from("HTTP/1.1 200 OK\r\n\r\n"),
-            "echo" => {
-                if let Some(param) = path_segments.nth(0) {
-                    let length = param.len();
-                    format!("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length:{length}\r\n\r\n{param}")
-                } else {
-                    String::from("HTTP/1.1 200 OK\r\n\r\n")
-                }
-            }
-            "user-agent" => {
-                if let Some(ua) = http_request
-                    .headers
-                    .iter()
-                    .find(|header| header.name.contains("User-Agent"))
-                {
-                    format!("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length:{}\r\n\r\n{}", ua.value.len(), ua.value)
-                } else {
-                    String::from("HTTP/1.1 200 OK\r\n\r\n")
-                }
-            }
-            _ => String::from("HTTP/1.1 404 Not Found\r\n\r\n"),
-        };
-    } else {
-        response = String::from("HTTP/1.1 404 Not Found\r\n\r\n");
+    for header in &request[1..] {
+        let (key, value) = header.split_once(":").unwrap();
+        request_headers.insert(key, value.trim());
     }
 
-    let _ = stream.write_all(response.as_bytes()).unwrap();
+    let path: Vec<&str> = request_line.split(" ").collect();
+    let path = path[1];
+
+    let response = if path == "/" {
+        Response::new(
+            String::from("200"),
+            String::from("OK"),
+            String::from("Content-Length: 0"),
+            String::new(),
+        )
+    } else if path.starts_with("/echo/") {
+        Response::create_response_with_body(&path[6..])
+    } else if path == "/user-agent" {
+        let user_agent = request_headers.get("User-Agent").unwrap();
+        Response::create_response_with_body(&user_agent)
+    } else if path.starts_with("/files/") {
+        let mut file_dir = env::args()
+            .skip_while(|arg| arg != "--directory")
+            .nth(1)
+            .expect("No Directory Argument provided!");
+        if !file_dir.ends_with("/") {
+            file_dir.push_str("/");
+        }
+        let path = format!("{file_dir}{}", &path[7..]);
+        Response::create_file_response(&path)
+    } else {
+        Response::not_found()
+    };
+
+    stream
+        .write_all(&response.create_http_response().unwrap())
+        .unwrap();
 }
