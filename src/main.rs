5b2b7ece9c534f08e8b555cc4a1bb8e8ded2fe59
nikicivan <ivan.nikic@gmail.com>
2024-10-25 15:58:51 +0200

    Read Header

--- src/main.rs
+++ src/main.rs
@@ -20,21 +20,38 @@ impl HttpRequestMethod {
 }
 
 #[derive(Debug)]
+struct HttpHeader<'a> {
+    name: &'a str,
+    value: &'a str,
+}
+
+#[derive(Debug)]
 struct HttpRequest<'a> {
     method: HttpRequestMethod,
     version: &'a str,
     path: &'a str,
+    headers: Vec<HttpHeader<'a>>,
 }
 
 impl<'a> HttpRequest<'a> {
-    pub fn new(request_line: &'a str) -> Self {
+    pub fn new(request_line: &'a str, header_lines: &'a [String]) -> Self {
         let split_line: Vec<_> = request_line.split_whitespace().collect();
         println!("New HTTP Request {:?}", split_line.clone());
+        let mut headers: Vec<HttpHeader<'a>> = Vec::new();
+        for header in header_lines {
+            if let Some((key, value)) = header.split_once(":") {
+                headers.push(HttpHeader {
+                    name: key.trim(),
+                    value: value.trim(),
+                });
+            }
+        }
         return Self {
             method: HttpRequestMethod::from_string(split_line[0])
                 .expect("Unable to parse http method"),
             version: split_line[2],
             path: split_line[1],
+            headers,
         };
     }
 }
@@ -73,7 +90,7 @@ fn handle_connection(mut stream: TcpStream) {
 
     println!("Request line {:#?}", request_line.clone());
 
-    let http_request = HttpRequest::new(&request_line[0]);
+    let http_request = HttpRequest::new(&request_line[0], &request_line[1..]);
     let response: String;
     let mut path_segments = http_request.path.split("/");
     if let Some(endpoint) = path_segments.nth(1) {
@@ -84,6 +101,17 @@ fn handle_connection(mut stream: TcpStream) {
                 if let Some(param) = path_segments.nth(0) {
                     let length = param.len();
                     format!("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length:{length}\r\n\r\n{param}")
+                } else {
+                    String::from("HTTP/1.1 200 OK\r\n\r\n")
+                }
+            }
+            "user-agent" => {
+                if let Some(ua) = http_request
+                    .headers
+                    .iter()
+                    .find(|header| header.name.contains("User-Agent"))
+                {
+                    format!("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length:{}\r\n\r\n{}", ua.value.len(), ua.value)
                 } else {
                     String::from("HTTP/1.1 200 OK\r\n\r\n")
                 }
